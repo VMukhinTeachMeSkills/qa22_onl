7ddacc9e64495040fa89be5d3063504dd6da5dfa
¨Viachaslau_Mukhin¨ <viachaslau.mukhin@expert-soft.by>
2023-07-04 21:00:23 +0300

    Lesson 3 change (from lesson4 branch)

--- src/lesson3/Homework1.java
+++ src/lesson3/Homework1.java
@@ -4,6 +4,6 @@ public class Homework1
 {
     public static void main(String[] args)
     {
-        System.out.println("Lesson 3 done");
+        System.out.println("Lesson 3 (changed in branch lesson4) done");
     }
 }
